e16a27a69e6ecac02c589e245080e9f2fb0795a8
Aarthi1899 <nssaarthi2002@gmail.com>
2024-03-01 10:28:51 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import "./globals.css";
+import React from "react";
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/context/ThemeProvider";
+
+export const metadata: Metadata = {
+  title: "CodeQuest",
+  description:
+    "A Community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with other students or developers.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
